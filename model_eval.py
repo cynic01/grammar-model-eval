809fe99158777a2abc724ba61bbc8691110eec63
Edison Chen <zc2738@columbia.edu>
2023-09-24 23:35:41 -0400

    rename and add conll14 test data

--- model_eval.py
+++ model_eval.py
@@ -19,30 +19,30 @@ class FileEval:
                  model_results_tokenized=False,
                  label=Path(wd / "label.txt"),
                  label_tokenized=False,
-                 model_m2_out=Path(wd / "model_m2_out.txt"),
-                 label_m2_out=Path(wd / "label_m2_out.txt")):
+                 model_m2_out=Path(wd / "model_edits.m2"),
+                 label_m2_out=Path(wd / "gold_edits.m2")):
         # convert to word tokenized format
         if not orig_tokenized:
             with open(orig, 'r') as forig:
                 orig_lines = forig.readlines()
             orig_tokens_text = [" ".join([token.text for token in self.tokenizer(sent.strip())]) + "\n" for sent in orig_lines]
-            orig = Path(wd / "orig_tmp.txt")
-            with open(orig, 'w') as ftmp:
-                ftmp.writelines(orig_tokens_text)
+            orig = Path(wd / "orig_tokenized.txt")
+            with open(orig, 'w') as f:
+                f.writelines(orig_tokens_text)
         if not model_results_tokenized:
             with open(model_results, 'r') as fmodel_results:
                 model_results_lines = fmodel_results.readlines()
             model_results_tokens_text = [" ".join([token.text for token in self.tokenizer(sent.strip())]) + "\n" for sent in model_results_lines]
-            model_results = Path(wd / "model_results_tmp.txt")
-            with open(model_results, 'w') as ftmp:
-                ftmp.writelines(model_results_tokens_text)
+            model_results = Path(wd / "model_results_tokenized.txt")
+            with open(model_results, 'w') as f:
+                f.writelines(model_results_tokens_text)
         if not label_tokenized:
             with open(label, 'r') as flabel:
                 label_lines = flabel.readlines()
             label_tokens_text = [" ".join([token.text for token in self.tokenizer(sent.strip())]) + "\n" for sent in label_lines]
-            label = Path(wd / "label_tmp.txt")
-            with open(label, 'w') as ftmp:
-                ftmp.writelines(label_tokens_text)
+            label = Path(wd / "label_tokenized.txt")
+            with open(label, 'w') as f:
+                f.writelines(label_tokens_text)
         # generate annotated m2 files
         proc_model = subprocess.run(["serrant_parallel", "-orig", str(orig), "-cor", str(model_results), "-out", str(model_m2_out)])
         proc_label = subprocess.run(["serrant_parallel", "-orig", str(orig), "-cor", str(label), "-out", str(label_m2_out)])
